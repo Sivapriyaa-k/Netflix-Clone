2088bf127a1f23feab2b8e0970c312ae914e0f8f
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-03-27 21:20:35 +0530

    27-03-2025

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./TitleCards.css";
 import title_cards from "../../assets/cards/Cards_data";
+
 function TitleCards() {
+  const cardsRef = useRef();
+
+  const handleWheel = (e) => {
+    e.preventDefault;
+    cardsRef.current.scrollLeft += e.deltaY;
+  };
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
   return (
     <div className="titlecards">
       <h2>Popular On Netflix</h2>
-      <div className="card-list">
+      <div className="card-list" ref={cardsRef}>
         {title_cards.map((card, index) => {
           return (
             <div className="card" key={index}>
