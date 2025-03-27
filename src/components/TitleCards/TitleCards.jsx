9f66ba9cb818c1e7be21e0c0b8de6b13cb14504b
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-03-27 21:08:31 +0530

    27-03-2025

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "./TitleCards.css";
+import title_cards from "../../assets/cards/Cards_data";
 function TitleCards() {
-  return <div className="titlecards"></div>;
+  return (
+    <div className="titlecards">
+      <h2>Popular On Netflix</h2>
+      <div className="card-list">
+        {title_cards.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default TitleCards;
