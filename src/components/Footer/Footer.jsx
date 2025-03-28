30ee4f233cee03d49d24fcfc74d5e391f5bd50bd
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-03-28 15:25:04 +0530

    28-03-2025

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "./footer.css";
+import youtube_icon from "../../assets/youtube_icon.png";
+import twitter_icon from "../../assets/twitter_icon.png";
+import instagram_icon from "../../assets/instagram_icon.png";
+import facebook_icon from "../../assets/facebook_icon.png";
+
 function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <div className="footer-icons">
+        <img src={youtube_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={facebook_icon} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
