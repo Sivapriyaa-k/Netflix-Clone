4ab447e5239b040f614e4eb8c767091b1e40aa8f
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-03-29 11:56:03 +0530

    29-03-2025

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,6 +14,21 @@ function Footer() {
         <img src={instagram_icon} alt="" />
         <img src={facebook_icon} alt="" />
       </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Center</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact US</li>
+      </ul>
+      <p className="copyright-text">© 1997-2025 Netflix, Inc.</p>
     </div>
   );
 }
