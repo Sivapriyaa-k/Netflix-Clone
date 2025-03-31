1c58a30cc9137dca0fe9c003359e6629c3ec3647
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-03-31 14:51:14 +0530

    31-03-2025

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,26 +1,48 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import title_cards from "../../assets/cards/Cards_data";
 
 function TitleCards({ title, category }) {
   const cardsRef = useRef();
+  const [apiData, setApiData] = useState([]);
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYjE1OTEzZGQxZjJiZThiNDVjMDk2ZmMwNzY0NDVlMiIsIm5iZiI6MTc0MzQwNjkwMC4xNzkwMDAxLCJzdWIiOiI2N2VhNDczNGFmNzUyYTNiMjRmNzEyZjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.PusX_RPnDEbJ0GEWJPXG1Encpb561bRjBladHPKw1pQ",
+    },
+  };
 
   const handleWheel = (e) => {
     e.preventDefault;
     cardsRef.current.scrollLeft += e.deltaY;
   };
   useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
   return (
     <div className="titlecards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {title_cards.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
 function Player() {
-  return <div className="player"></div>;
+  return (
+    <div className="player">
+      <img src={back_arrow_icon} alt="" />
+      <iframe
+        width="90%"
+        height="90%"
+        src="https://www.youtube.com/embed/YQQD67N5pi0"
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>Published Data</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
+    </div>
+  );
 }
 
 export default Player;
