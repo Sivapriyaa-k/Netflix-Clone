be72b7be38de50df7f81b5c8a7e560cd80272e37
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-04-01 10:41:05 +0530

    01-04-2025

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+    apiKey: "AIzaSyAwIHRKEQ_K7RlLbctlHfuLADNKQIMNpcs",
+    authDomain: "netflix-clone-ff54d.firebaseapp.com",
+    projectId: "netflix-clone-ff54d",
+    storageBucket: "netflix-clone-ff54d.firebasestorage.app",
+    messagingSenderId: "500745063104",
+    appId: "1:500745063104:web:3194b8d356c22662a77b25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+const signup = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+    } catch (error) {
+
+    }
+}
